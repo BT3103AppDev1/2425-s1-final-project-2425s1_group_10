548f80a808fed423124741629d68c986cdcc5d29
Wanft11 <163952364+Wanft11@users.noreply.github.com>
2024-11-12 00:13:16 +0800

    Updated firebase

--- src/models/Supplier.js
+++ src/models/Supplier.js
@@ -1,16 +1,17 @@
 // src/models/Supplier.js
 
 export default class Supplier {
-    constructor(supplier_id, supplier_name, contact_person, phone_number, email, products_supplied, lead_time, status, last_order_date) {
-      this.id = supplier_id;
-      this.name = supplier_name;
-      this.contactPerson = contact_person;
-      this.phoneNumber = phone_number;
-      this.email = email;
-      this.product = products_supplied;
-      this.leadTime = lead_time;
-      this.status = status;
-      this.lastOrderDate = last_order_date;
-    }
+  constructor(suppliername, supplierid, contactperson, phonenumber, email, productssupplied, leadtime, status, lastorderdate) {
+    this.suppliername = suppliername;              // Supplier ID
+    this.supplierid = supplierid;  // Supplier Name
+    this.contactperson = contactperson;  // Contact Person
+    this.phonenumber = phonenumber;    // Phone Number
+    this.email = email;                // Email Address
+    this.productssupplied = productssupplied;  // Products Supplied
+    this.leadtime = leadtime;          // Lead Time
+    this.status = status;              // Supplier Status
+    this.lastorderdate = lastorderdate;  // Last Order Date
   }
+}
+
   
